c48c3f7f45cf40f5282cd1cb4f61dbbe2509bf55
hyh <dfhjdfh@163.com>
2016-10-26 15:57:51 +0800

    10-25

--- public/javascripts/Item.js
+++ public/javascripts/Item.js
@@ -10,3 +10,25 @@ console.log(sub);
 
 
 });
+
+$("img").each(function(){
+        $(this).click(function(){
+            var falg = true;
+           if(falg){
+               falg = false;
+               $(this).height("250px");
+               $(this).width("200px");
+
+
+           }else{
+               $(this).height("50px");
+               $(this).width("40px");
+
+
+
+           }
+
+        });
+
+
+});
--- public/javascripts/merchart_details.js
+++ public/javascripts/merchart_details.js
@@ -3,11 +3,92 @@
         location.href = '/';
     }
 }*/
-$("#newpage").click(function(){
+
+  /*  var data= [{
+            id:"dsdsa",
+            name: "assd",
+            contact: "ass",
+            state: "open",
+        },
+        {
+            id:"5154",
+            name: "mnbv",
+            contact: "zas",
+            state: "open",
+        },
+        {
+            id:"yutu",
+            name: "lijdf",
+            contact: "zxcc",
+            state: "open",
+        }
+
+    ]
+*/
+
+
+ $(document).ready(function () {
+     $.get("http://" + backend_host + '/web/admin/manage/shop?access_token=11a75c2681eb7ee5f0d0873ac2dfa6f1',
+         {
+             "page":0 ,
+             "limit": 5,
+         },
+         function (data) {
+                 var tbody = "";
+                 $.each(data, function (i, order) {
+                     tbody = '<tr><td><a href="../pages/examples/invoice.html">' + order.id + '</a></td>'
+                     tbody += '<td>' + order.name + '</td>'
+                     tbody += '<td><span class="label label-success">' + order.contact + '</span></td>'
+                     tbody += '<td><div class="sparkbar" data-color="#00a65a" data-height="20">' + order.state + '</div></td>'
+                     tbody += '<td><span class="label label-info"><a href="/merchart/details">编辑</a></span></td></tr>'
+
+
+                     $('#Table').find('tbody').append(tbody);
+
+                 });
+
+
+
+             $('.pagination').append(' <li class="active"><a href="#Table">1<span class="sr-only">(current)</span></a></li>');
+             $('.pagination').append('<li><a href="#">2</a></li>');
+             $('.pagination').append('<li><a href="#">3</a></li>');
+             $('.pagination').append('<li><a href="#">4</a></li>');
+             $('.pagination').append('<li class="disabled"><a href="#">&raquo;</a></li>');
+
+
+
+         })
+     });
+
+$('.pagination:eq(0)').on('click','li',function(){
+    $(this).addClass('active').siblings().removeClass('active');
+    console.log($(this).index());
+        var index = $(this).index() - 1;
     $.get("http://" + backend_host + '/web/admin/manage/shop?access_token=11a75c2681eb7ee5f0d0873ac2dfa6f1',
+        {
+            "page":index,
+            "limit":5
+        },
         function (data) {
-            console.log(data);
-            // location.href= '/admin';
-            // localStorage.setItem('caishengu-access_token', data.access_token);
+console.log(data);
+            var tbody = "";
+            $.each(data, function (i, order) {
+                tbody = '<tr><td><a href="../pages/examples/invoice.html">' + order.id + '</a></td>'
+                tbody += '<td>' + order.name + '</td>'
+                tbody += '<td><span class="label label-success">' + order.contact + '</span></td>'
+                tbody += '<td><div class="sparkbar" data-color="#00a65a" data-height="20">' + order.state + '</div></td>'
+                tbody += '<td><span class="label label-info"><a href="/merchart/details">编辑</a></span></td></tr>'
+
+
+
+
+            });
+            $('#Table').find('tbody').html(tbody);
+
         })
-});
+
+
+})
+
+
+
