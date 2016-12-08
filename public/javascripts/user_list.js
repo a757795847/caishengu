630d20213e2cee10151a3ab024f43950cf13dde2
zlm <qazse41@sina.cn>
2016-12-08 11:30:53 +0800

    save

--- public/javascripts/user_list.js
+++ public/javascripts/user_list.js
@@ -1,14 +1,17 @@
 var lost = localStorage.getItem('user_list').split(',');
 
-var liLength = $('.sidebar .sidebar-menu li').length;
+var liLength = $('.sidebar .sidebar-menu .treeview').length;
+
 
 for(var i=0;i<lost.length;i++){
     for(var j =0;j<liLength;j++){
-        if($('.sidebar .sidebar-menu li').eq(j).attr('data-token') == lost[i]){
-            $('.sidebar .sidebar-menu li').eq(j).removeClass('user-list-hide');
+        if($('.sidebar .sidebar-menu .treeview').eq(j).attr('data-token') == lost[i]){
+            $('.sidebar .sidebar-menu .treeview').eq(j).removeClass('user-list-hide');
         }
     }
 }
+
+
 
 function errorMessage(text) {
     $('#error').text(text);
