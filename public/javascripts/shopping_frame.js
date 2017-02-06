399cd50b93e937af7d11f00331f47d26a5cbf116
zlm <qazse41@sina.cn>
2017-02-06 15:58:26 +0800

    save

--- public/javascripts/shopping_frame.js
+++ public/javascripts/shopping_frame.js
@@ -10,3 +10,23 @@ $("#addgr").click(function(){
 
 
 });
+
+
+$(document).ready(function () {
+   $.get("http://" + backend_host + '/web/staff/goods/market/class/collection?' + token,
+       function (data) {
+          console.log(data)
+          var tbody = "";
+
+          $.each(data.list, function (i, order) {
+             tbody +='<tr> <td><img src="" alt="">'+order.name+'</td>';
+             tbody +='<td><a href="/shopping/frame/detail">编辑</a></td>';
+             tbody +='<td><span><a href="#" data-toggle="modal" data-target="#myModal">删除</a></span></td>';
+             tbody +='<td><span>上</span><span class="bottom">下</span></td></tr>';
+
+          });
+
+          $('#shopping_frame_table').find('tbody').append(tbody);
+          console.info("tbody", tbody)
+       })
+});
