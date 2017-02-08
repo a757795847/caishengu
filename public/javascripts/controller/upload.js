34c3bd01d65498ddf16de9681e6e51f15f887bf9
zlm <qazse41@sina.cn>
2017-02-08 10:56:53 +0800

    update: file update module

--- public/javascripts/controller/upload.js
+++ public/javascripts/controller/upload.js
@@ -1,116 +1,92 @@
-var images = '', imagetoken =[];
-$('#pickfiles').on('click',function(){
-    imagetoken = [];
-    imagetokens();
-})
-function imagetokens(){
+function imagetokens() {
+    var result = null;
     $.ajax({
-        type:'POST',
-        url:'http://' + backend_host + '/other/file/apply?'+token,
-        dataType:'json',
-        async:false,
-        success:function(data){
+        type: 'POST',
+        url: 'http://' + backend_host + '/other/file/apply?' + token,
+        dataType: 'json',
+        async: false,
+        success: function (data) {
 
-            imagetoken.push(data.token);
-            imagetoken.push(data.file_key);
-
+            /*imagetoken.push(data.token);
+            imagetoken.push(data.file_key);*/
+            result = data;
+            console.log("imagetokens", data)
         },
-        error:function(jqXHR){
-            if(jqXHR.status == 400){
+        error: function (jqXHR) {
+            if (jqXHR.status == 400) {
 
             }
         }
     });
+    return result;
 }
-imagetokens();
-//创建上传七牛
 
-var uploader = Qiniu.uploader({
-    runtimes: 'html5,flash,html4',
-    browse_button: 'container',
-    container: 'addImgs',
-    uptoken: imagetoken[0],
-    unique_names: false,
-    multi_selection: false,
-    max_file_size:'3mb',
-    save_key: false,
-    domain: 'http://qiniu-plupload.qiniudn.com/',
-    get_new_uptoken: true,
-    auto_start: true,
-    log_level: 5,
-    filters: {
-        mime_types: [
-            {title: "Image files", extensions: "jpg,jpeg,gif,png"}
-        ]
-    },
-    init: {
-        'FilesAdded': function (up, files) {
-
+function newQiniu(completeCallback, browse_button, container, token) {
+    return Qiniu.uploader({
+        runtimes: 'html5,flash,html4',
+        browse_button: browse_button, //'container',
+        container: container,// 'addImgs',
+        uptoken: token,
+        unique_names: false,
+        multi_selection: false,
+        max_file_size: '3mb',
+        save_key: false,
+        domain: 'http://qiniu-plupload.qiniudn.com/',
+        get_new_uptoken: true,
+        auto_start: true,
+        log_level: 5,
+        filters: {
+            mime_types: [
+                {title: "Image files", extensions: "jpg,jpeg,gif,png"}
+            ]
         },
-        'BeforeUpload': function (up, file) {
+        init: {
+            'FilesAdded': function (up, files) {
 
-        },
-        'UploadProgress': function (up, file) {
+            },
+            'BeforeUpload': function (up, file) {
 
-        },
-        'UploadComplete': function () {
+            },
+            'UploadProgress': function (up, file) {
 
-        },
-        'FileUploaded':
-           function (up, file, info) {
-            images = imagetoken[1];
-            var domain = up.getOption('domain');
-            var res = eval('(' + info + ')');
-            var Src = 'http://' + backend_host + '/other/file/' + res.key + '?' + token;
-            var imageBoxs = '';
-            imageBoxs += '<div class="imgBox"><button type="button" data-name="'+ imagetoken[1] +'" class="close" data-dismiss="alert" aria-hidden="true">×</button>';
-            imageBoxs += '<img src="'+ Src +'"></div>';
-            $('#container').hide();
+            },
+            'UploadComplete': function () {
 
+            },
+            'FileUploaded': function (up, file, info) {
+                console.info("FileUploaded", up, file, info);
+                var key = JSON.parse(info).key;
+                //images = imagetoken[1];
+                //var domain = up.getOption('domain');
+                //var res = eval('(' + info + ')');
+                var src = 'http://' + backend_host + '/other/file/' + key + '?' + token;
+                completeCallback(key, src);
+            },
+            'Error': function (up, err, errTip) {
 
-            
-            $('#container').before(imageBoxs);
-            console.log(images);
-        },
-        'Error': function (up, err, errTip) {
-
+            }
+            ,
+            'Key': function (up, file) {
+                var key = imagetokens().file_key;
+                console.info("Key.function: file_key" + key);
+                return key;
+            }
         }
-        ,
-        'Key': function (up, file) {
-            var key = imagetoken[1];
-            return key;
-        }
-    }
-});
-
-$('#fsUploadProgress').on('mousemove ','.imgBox',function(){
-    $(this).find('button').css('display','block');
-
-})
-$('#fsUploadProgress').on('mouseout ','.imgBox',function(){
-    $(this).find('button').css('display','none');
-})
-$('#fsUploadProgress').on('click ','.imgBox button',function(e){
-    e.stopPropagation();
-    var dataName = $(this).attr('data-name');
-    images = '';
-    $(this).parent().remove();
-    $('#container').show();
-})
-
+    });
+}
 
 //   通知图片上传成功
-function imgUPload(imgId){
+function imgUPload(imgId) {
     $.ajax({
-        type:'PUT',
-        url:'http://' + backend_host + '/other/file/'+imgId+'?'+token,
-        dataType:'json',
-        async:false,
-        success:function(data){
+        type: 'PUT',
+        url: 'http://' + backend_host + '/other/file/' + imgId + '?' + token,
+        dataType: 'json',
+        async: false,
+        success: function (data) {
             console.log(data);
         },
-        error:function(jqXHR){
-            if(jqXHR.status == 400){
+        error: function (jqXHR) {
+            if (jqXHR.status == 400) {
 
             }
         }
