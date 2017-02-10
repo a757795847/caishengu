30936722d4d9b96bf72ec01bee56f6bb82633418
lichen <392950356@qq.com>
2017-02-10 19:31:20 +0800

    商城货架、贡品货架

--- public/javascripts/shopping_frame.js
+++ public/javascripts/shopping_frame.js
@@ -5,7 +5,7 @@ $("#addtr").click(function(){
 
 });
 $("#addgr").click(function(){
-   window.location.href="/shopping/frame/details";
+   window.location.href="/shopping/frame/details?0";
 
 
 
@@ -13,11 +13,11 @@ $("#addgr").click(function(){
 
 
 $(document).ready(function () {
+
    $.getJSON("http://" + backend_host + '/web/staff/goods/market/class/collection?' + token,
        function (data) {
           console.log(data)
           var tbody = "";
-
           $.each(data.list, function (i, order) {
              var imageSrc = "http://" + backend_host + order.image + "?" + token
              tbody +='<tr> <td><img src= '+ imageSrc +' alt="">'+order.name+'</td>';
@@ -26,11 +26,20 @@ $(document).ready(function () {
              tbody +='<td><span>上</span><span class="bottom">下</span></td></tr>';
 
           });
-
           $('#shopping_frame_type_table').find('tbody').append(tbody);
           // console.info("tbody", tbody)
+
+
+           var list_tbody = "";
+           $.each(data.list, function (i, order) {
+               list_tbody +='<option value='+i+'>'+order.name+'</option>';
+
+           });
+           console.info("list_tbody", list_tbody);
+           console.info("$('#shopping_frame_broad_heading')", $('#shopping_frame_broad_heading'));
+           $('select.shopping_frame_broad_heading').append(list_tbody);
        });
-   
+
    $.getJSON("http://" + backend_host + '/web/staff/goods/market/collection?' + token,
        function (data) {
           console.log(data)
@@ -46,7 +55,7 @@ $(document).ready(function () {
 
              tbody +='<tr> <td>'+order.given_id+'</td>';
              tbody +='<td> '+ order.name +'</td>';
-             tbody +='<td> '+ order.price_point +' 财神币/ '+ order.price_coin +' 积分</td>';
+             tbody +='<td> '+ order.price_point +'财神币/ '+ order.price_coin +'积分 </td>';
              tbody +='<td>  <a href="/shopping/frame/details?id='+ order.id +'">编辑</a></td>';
              tbody +='<td>  <span> '+recommend +'</span></td>';
              tbody +='<td>  <span> 上架 </span></td></tr>';
